3cc8ce120a405e25bb67e0f4c53c86c9dc8ce615
Ricky Catron <catron.ricky@gmail.com>
2016-11-08 18:44:30 -0500

    Moved game code to seperate file

---
+++ gamescript.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ricky on 11/8/2016.
+ */
+function startNewGame() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create});
+
+    function preload() {
+
+    }
+
+    function create() {
+
+    }
+}
