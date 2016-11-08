5edcec8e5d6d930c811ad4bb47d79c4186c4a6f5
Ricky Catron <catron.ricky@gmail.com>
2016-11-08 18:53:05 -0500

    game preloads audio and start when button that doesnt exist is clicked

--- gamescript.js
+++ gamescript.js
@@ -1,12 +1,15 @@
 /**
  * Created by Ricky on 11/8/2016.
  */
-function startNewGame() {
+
+var start_button = document.getElementById("start_button");
+
+start_button.onclick = function() {
 
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create});
 
     function preload() {
-
+        game.load.audio("song1", "Scott_Holmes_Paint_By_Numbers.mp3");
     }
 
     function create() {
