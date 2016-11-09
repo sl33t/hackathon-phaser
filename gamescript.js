8c85c4ed45fd9ec18ec176ddd5ddf6c02dc3d336
Ricky Catron <catron.ricky@gmail.com>
2016-11-08 20:30:41 -0500

    Added keyup and down info

--- gamescript.js
+++ gamescript.js
@@ -14,21 +14,67 @@ start_button.onclick = function() {
 
     function create() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        document.onkeydown = keyChange;
+        document.onkeyup = keyChange;
+
         game.stage.backgroundColor = '#FFFFFF';
 
         var song1 = game.add.audio("song1");
 
         songs = [song1];
+        notes = game.add.group();
+        keys = game.add.group();
 
         game.sound.setDecodedCallback(songs, startGame, this);
     }
 
     function update() {
-
+        //var hitPlatform = game.physics.arcade.overlap(notes, keys, ensure_same_type, note_hit);
     }
 }
 
 function startGame() {
     var song1 = songs.shift();
     song1.play();
+}
+
+function ensure_same_type() {
+
+}
+
+function note_hit() {
+
+}
+
+function keyChange(event) {
+    console.log(event.type);
+    switch (event.keyCode) {
+        //65 = a
+        case 65:
+            break;
+        //83 = s
+        case 83:
+            break;
+        //68 = d
+        case 68:
+            break;
+        //70 = f
+        case 70:
+            break;
+        //74 = j
+        case 74:
+            break;
+        //75 = k
+        case 75:
+            break;
+        //76 = l
+        case 76:
+            break;
+        //186 = ;
+        case 186:
+            break;
+        //13 = enter
+        case 13:
+            break;
+    }
 }
