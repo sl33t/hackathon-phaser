39dc00811e9ba7ec35c17605f07eeaed5c377fe2
Ricky Catron <catron.ricky@gmail.com>
2016-11-08 19:58:42 -0500

    Basics of game code

--- gamescript.js
+++ gamescript.js
@@ -6,13 +6,29 @@ var start_button = document.getElementById("start_button");
 
 start_button.onclick = function() {
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create});
+    game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
     function preload() {
         game.load.audio("song1", "Scott_Holmes_Paint_By_Numbers.mp3");
     }
 
     function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#FFFFFF';
+
+        var song1 = game.add.audio("song1");
+
+        songs = [song1];
+
+        game.sound.setDecodedCallback(songs, startGame, this);
+    }
+
+    function update() {
 
     }
+}
+
+function startGame() {
+    var song1 = songs.shift();
+    song1.play();
 }
